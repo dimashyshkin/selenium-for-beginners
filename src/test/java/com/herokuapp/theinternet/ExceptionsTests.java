987d65b24b70062e400267fa20964fdaa40c35ce
dimashyshkin <dimashyshkin@hotmail.com>
2018-05-13 12:41:55 -0400

    exceptions

--- src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
+++ src/test/java/com/herokuapp/theinternet/ExceptionsTests.java
@@ -1,9 +1,16 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ExceptionsTests {
 	WebDriver driver;
@@ -19,6 +26,97 @@ public class ExceptionsTests {
 
 		driver.manage().window().maximize();
 		return driver;
+	}
+
+	@Test
+	public void notVisibleTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement startButton = driver.findElement(By.xpath("//button"));
+		startButton.click();
+
+		WebElement finishText = driver.findElement(By.id("finish"));
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOf(finishText));
+
+		Assert.assertTrue(finishText.getText().equals("Hello World!"),
+				"'Hello World! text is not present on the page.'");
+
+		startButton.click();
+	}
+
+	@Test
+	public void timeoutTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_loading/1";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement startButton = driver.findElement(By.xpath("//button"));
+		startButton.click();
+
+		WebElement finishText = driver.findElement(By.id("finish"));
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+		try {
+			wait.until(ExpectedConditions.visibilityOf(finishText));
+		} catch (TimeoutException e) {
+			// Steps after exception
+			System.out.println("Exception catched: " + e.getMessage());
+			sleep(3000);
+		}
+
+		Assert.assertTrue(finishText.getText().equals("Hello World!"),
+				"'Hello World! text is not present on the page.'");
+	}
+
+	@Test
+	public void noSuchElementTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_loading/2";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement startButton = driver.findElement(By.xpath("//button"));
+		startButton.click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		Assert.assertTrue(
+				wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("finish"), "Hello World!")),
+				"Failed to wait for text to be present in element.");
+
+		// WebElement finishText = driver.findElement(By.id("finish"));
+		// Assert.assertTrue(finishText.getText().equals("Hello World!"),
+		// "'Hello World! text is not present on the page.'");
+
+	}
+
+	@Test
+	public void staleElementTest() {
+		// open the page
+		String url = "http://the-internet.herokuapp.com/dynamic_controls";
+		driver.get(url);
+		sleep(2000);
+		System.out.println("Page is opened.");
+
+		WebElement button = driver.findElement(By.xpath("//button"));
+		WebElement checkbox = driver.findElement(By.id("checkbox"));
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		button.click();
+		Assert.assertTrue(wait.until(ExpectedConditions.stalenessOf(checkbox)),
+				"Checkbox is still displayed, but it shouldn't be");
+		// WebElement loading = driver.findElement(By.id("loading"));
+		// wait.until(ExpectedConditions.invisibilityOf(loading));
+
+		// Assert.assertFalse(checkbox.isDisplayed(), "Checkbox is displayed, but it
+		// should not be.");
 	}
 
 	@AfterMethod
